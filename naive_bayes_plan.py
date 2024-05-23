0f296aa3b23dfaffa7562168f176c13ab5e3b3ac
JoeZiminski <Joseph.j.ziminski@gmail.com>
2024-05-23 15:33:39 +0100

    Add naive bayes plan.

---
+++ naive_bayes_plan.py
@@ -0,0 +1,15 @@
+
+# Categorical
+
+# use mnist dataset, predict between all 9!
+
+
+# Multinomial
+
+# use a made up email example
+
+
+# Continous
+
+# try and fit 2d poisson. See blitzstein book and
+# https://en.wikipedia.org/wiki/Poisson_distribution#Bivariate_Poisson_distribution
